0595b70c96861f909c8ed4ca12a0dcaf6f2ac7a3
Tobias Halfar <tobias.j.halfar@gmail.com>
2023-09-06 20:42:45 +0300

    tulostetaan kukin luokka koosteena

--- klusterointi_keskussanoineen.py
+++ klusterointi_keskussanoineen.py
@@ -8,7 +8,7 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from nltk.corpus import stopwords
 import re
-from scipy.sparse import csr_matrix
+from collections import defaultdict
 
 documents = ["Koira juoksee", "Kissa kiipeää", "Lintu lentää",
              "Kala ui", "Koira haukkuu", "Kissa naukuu",
@@ -29,7 +29,7 @@ documents = [preprocess(doc) for doc in documents]
 vectorizer = TfidfVectorizer()
 X_reduced = vectorizer.fit_transform(documents)
 
-# Suorita PCA-analyysi (valinnainen)
+# Suorita PCA-analyysi 
 pca = PCA(n_components=5)
 X_reduced = pca.fit_transform(X_reduced.toarray())
 
@@ -40,6 +40,14 @@ kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init="auto").fit(X_redu
 # Määritä, mihin klusteriin kukin datakohta kuuluu
 labels = kmeans.labels_
 
-# Tulosta klusterilabelit jokaista tekstiä kohden
+# Ryhmittele dokumentit klusterilabeleihinsa perustuen
+clustered_documents = defaultdict(list)
+
 for i, label in enumerate(labels):
-    print(f'Teksti: "{documents[i]}" kuuluu klusteriin {label}')
+    clustered_documents[label].append(documents[i])
+
+# Tulosta dokumentit klusterikohtaisesti
+for label in sorted(clustered_documents.keys()):
+    print(f"\nKlusteri {label}:")
+    for doc in clustered_documents[label]:
+        print(f'  Teksti: "{doc}"')
