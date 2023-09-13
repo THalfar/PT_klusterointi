76dd74d23be72935992d89b0607e832c07335986
Tobias Halfar <tobias.j.halfar@gmail.com>
2023-09-13 22:27:28 +0300

    Changed some text..

--- panda_groupby_test.py
+++ panda_groupby_test.py
@@ -3,14 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 time_interval = "7D"
-start_time = "2023-02-01"
-end_time = "2023-04-01"
+start_time = "2023-05-01"
+end_time = "2023-09-15"
 
-time_col = pd.date_range(start='2023-01-01', end='2023-04-30', freq='D').to_list() * 3
+time_col = pd.date_range(start='2023-01-01', end='2023-12-30', freq='D').to_list() * 3
 group_col = np.random.choice(['A', 'B', 'C'], size=len(time_col))
 value = np.random.randint(6, 11, size=len(time_col))
 
-# Create a DataFrame
 data = {
     'time_column': time_col,
     'group_column': group_col,
@@ -38,5 +37,5 @@ for name, group in df.groupby('group_column'):
 plt.xlabel('Time')
 plt.ylabel('Value')
 plt.legend(title='Groups')
-plt.title(f'Weekly Sum of Different Groups by interval: {time_interval}')
+plt.title(f'Sum of Different Groups time: {start_time} - {end_time} Interval: {time_interval}')
 plt.show()
